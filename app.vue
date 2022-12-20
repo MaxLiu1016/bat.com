0e45a53ebd6b9f8597deeb4149feaf584fd67a74
Max.l <a0958057936@gmail.com>
2022-12-20 18:46:57 +0800

    test: test commit

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
         class="px mx-auto grid grid-cols-12 sm:px-6 lg:max-w-7xl lg:gap-5 lg:px-8"
       >
         <!-- left sidebar -->
-        <div class="px">123</div>
+        <div class="px">1234</div>
         <!-- main content -->
         <!-- right sidebar -->
       </div>
